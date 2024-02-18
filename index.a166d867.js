1b4ba7f6150c4726ecf7f30ac9433c1a789d10e5
SimaMNL <144942961+SimaMNL@users.noreply.github.com>
2024-02-18 11:45:03 +0000

    Deploying to gh-pages from @ SimaMNL/Fresh-Harvest-Box@573b55d8c23eef6e411a22716b1f16d0ac3d8c63 🚀

---
+++ index.a166d867.js
@@ -0,0 +1,2 @@
+(()=>{let e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-order]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.a166d867.js.map
